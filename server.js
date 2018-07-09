28233a299a01091837032a2bc12def19ac308548
Lucas Stefanski <stefaluc000@gmail.com>
2018-07-09 15:29:52 -0500

    Initial scaffolding

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const data = require('./profiles');
+
+const index = require('./routes/index');
+const profiles = require('./routes/profiles');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// parse http request body
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+//app.use('/api/', profiles);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
